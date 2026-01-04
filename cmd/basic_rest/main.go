cf1009cd42e424fe70efe62fa94930f9d77e3448
Muntaha <muntahasayed396@gmail.com>
2026-01-04 17:40:11 +0530

    Gracefully shutdown

--- cmd/basic_rest/main.go
+++ cmd/basic_rest/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Muntaha369/Go_REST/internals/config"
 )
@@ -23,10 +28,29 @@ func main() {
 	}
 
 	fmt.Println("server started")
-	
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt)
+
+	go func(){
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal("Failed to start")
 	}
+	}()
 
+	<- done
+
+	slog.Info("Shuting down the server")
+
+	ctx,cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("Failed to shut down the server", "error", err)
+	}
+
+	slog.Info("Shutdown server Gracefully")
 }
