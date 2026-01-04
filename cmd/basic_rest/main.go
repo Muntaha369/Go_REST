a8c43aec5ec24f852056888946305f8fb22c006d
Muntaha <muntahasayed396@gmail.com>
2026-01-04 16:27:31 +0530

    Created a server

--- cmd/basic_rest/main.go
+++ cmd/basic_rest/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Muntaha369/Go_REST/internals/config"
+)
 
 func main() {
-	fmt.Println("Hello this is from rest")
+	cfg := config.Mustload()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to REST api"))
+	})
+
+	server :=http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Println("server started")
+	
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start")
+	}
+
 }
