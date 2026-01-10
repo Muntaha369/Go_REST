7ce8e1fda8d6fdfdb8f122d57c36bce1521971a9
Muntaha <muntahasayed396@gmail.com>
2026-01-10 17:56:33 +0530

    Done commenting storage.go

--- internals/storage/storage.go
+++ internals/storage/storage.go
@@ -3,6 +3,7 @@ package storage
 import gtypes "github.com/Muntaha369/Go_REST/internals/types"
 
 type Storage interface {
+	//This are all the methods that can be used by variables type storage 
 	CreateUser(name string, email string, password string) (int64, error)
 	GetUserById(id int64) (gtypes.User, error)
 	GetUserList()([]gtypes.User, error)
